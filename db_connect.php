a17cc7efb1626eaacc164383390a2f761080c1e6
yoshida <c.yoshida.sys22@morijyobi.ac.jp>
2023-02-10 12:33:46 +0900

    db_connect.phpの作成、css・imgフォルダの作成を行いました。

---
+++ db_connect.php
@@ -0,0 +1,31 @@
+<?php
+
+//ユーザー名
+$user = "root";
+//パスワード
+$pass = "";
+//データベース名
+$database = "teamprojectB";
+//サーバー名
+$server = "localhost:3308";
+
+//DNS文字列の生成
+$dns = "mysql:host={$server};dbname={$database};charset=utf8";
+
+
+//mysql
+try{
+    //PDOクラスのインスタンスを作成してDBに接続する
+    $pdo = new PDO($dns,$user,$pass);
+    //プリペアドステートメントのエミュレーションを無効化
+    $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES,false);
+    //例外がスローされるようにする
+    $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+    echo "データベースに接続しました";
+}catch(Exception $e){
+    echo "DB接続エラー:";
+    // echo $e->getMessage;
+    exit();
+}
+
+?>
