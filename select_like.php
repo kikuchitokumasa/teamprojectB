2b0c60d37191f982f998f06eabf5b2501dcfdc11
yoshida <c.yoshida.sys22@morijyobi.ac.jp>
2023-02-15 23:35:04 +0900

    キーワード検索機能の実装。 キーワード検索機能と結果表示画面を作成しました。

--- select_like.php
+++ select_like.php
@@ -1,61 +1,32 @@
 <?php
-    //session start
     session_start();
+    require_once "db_connect.php";
 
-    //データベース接続用ファイルを読み込む
-    require_once 'db_connect.php';
+    $keyword = $_POST["keyword"];
+    $key = '%'.$keyword.'%';
 
-    if(!empty($_GET["user_id"])){
-        $_SESSION["user_id"] = $_GET["user_id"];
-    }
-    $user_id = $_SESSION["user_id"];
-    
-    $sql = "SELECT * FROM blog WHERE user_id = :user_id AND deletes = 0";
+    $sql = "SELECT * FROM blog WHERE user_id = :user_id AND deletes = 0 AND theme LIKE :keyword";
 
     $stm = $pdo->prepare($sql);
-    $stm->bindValue(':user_id', $user_id, PDO::PARAM_INT);
+    $stm->bindValue(':user_id', $_SESSION["user_id"], PDO::PARAM_INT);
+    $stm->bindValue(':keyword', $key, PDO::PARAM_STR);
     $stm->execute();
     $result = $stm->fetchAll(PDO::FETCH_ASSOC);
-    
-    $sql = "SELECT * FROM account WHERE user_id = :user_id";
 
-    $stm = $pdo->prepare($sql);
-    $stm->bindValue(':user_id', $user_id, PDO::PARAM_INT);
-    $stm->execute();
-    $account_result = $stm->fetchAll(PDO::FETCH_ASSOC);
-    if(!empty($account_result)){
-        $_SESSION["user_name"] = $account_result[0]["user_name"];
-    }
-    $user_name = $_SESSION["user_name"];
-
-
-    $_SESSION["blog_id"] = [];
-    $_SESSION["title"] = [];
-    $_SESSION["text"] = [];
-    $_SESSION["release"] = [];
-    $_SESSION["theme"] = [];
-
-    $_SESSION["error_title"] = [];
-    $_SESSION["error_text"] = [];
-    $_SESSION["error_release"] = [];
-    $_SESSION["error_theme"] = [];
- ?>
-
+?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>管理 トップページ</title>
-    <link rel="stylesheet" href="node_modules/ress/ress.css">
-    <!-- reset.css ress -->
+    <title>Document</title>
     <link rel="stylesheet" href="https://unpkg.com/ress/dist/ress.min.css" />
     <link rel="stylesheet" href="./css/style.css">
     <link rel="stylesheet" href="./css/management_style.css">
 </head>
 <body>
-    <header class="header1">
+<header class="header1">
         <div class="header1_container">
             <a href="management_top.php"><img src="./img/logo.png" alt=""></a>
             
@@ -71,29 +42,33 @@
         </svg>
     </header>
     
-    <div class="mana_d_main_container">
-        <div class="mana_d_left_container">
-            <div class="mana_d_circle"><a href="">新規投稿</a></div>
+    <div class="select_like_main_container">
+        <div class="select_like_left_container">
+            <div class="select_like_circle"><a href="">新規投稿</a></div>
             <p><?php echo $_SESSION["user_name"]; ?>さん</p>
             <img src="./img/girl.png" alt="img">
         </div>
 
-        <div class="mana_d_right_container">
-            <h1>一覧</h1>
+        <div class="select_like_right_container">
+            <h1>検索結果:<?php echo $keyword ?></h1>
             <table>
         <?php
             foreach($result as $data) {
                 echo<<<"EOD"
                         <tr>
-                            <td class="mana_d_title"><a href="">{$data['title']}</a></td>
-                            <td class=""><a class="mana_d_buttom mana_d_update" href="update.php?blog_id={$data['blog_id']}">編集</a></td>
-                            <td class=""><a class="mana_d_buttom mana_d_delete" href="delete.php?blog_id={$data['blog_id']}">削除</a></td>
+                            <td class="select_like_title"><a href="">{$data['title']}</a></td>
+                            <td class=""><a class="select_like_buttom select_like_update" href="update.php?blog_id={$data['blog_id']}">編集</a></td>
+                            <td class=""><a class="select_like_buttom select_like_delete" href="delete.php?blog_id={$data['blog_id']}">削除</a></td>
                         </tr>
                 EOD;
             }    
         ?>
-            </table>    
+            </table>
+            <div class="selet_like_buttom">
+                <a href="management_top.php">トップページへ</a>
+            </div>    
         </div>
+        
     </div>
 
     <footer class="footer1">
